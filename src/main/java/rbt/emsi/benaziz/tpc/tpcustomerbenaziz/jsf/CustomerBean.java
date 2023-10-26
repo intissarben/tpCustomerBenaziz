ce65b2ecad57f3581738dcae1d225ab9e3198240
Intissar BENAZIZ <Intissar BENAZIZ@DESKTOP-RRJUMKP>
2023-10-26 22:33:25 +0100

    Ajout jsf

---
+++ src/main/java/rbt/emsi/benaziz/tpc/tpcustomerbenaziz/jsf/CustomerBean.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package rbt.emsi.benaziz.tpc.tpcustomerbenaziz.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import rbt.emsi.benaziz.tpc.tpcustomerbenaziz.entity.Customer;
+import rbt.emsi.benaziz.tpc.tpcustomerbenaziz.service.CustomerManager;
+
+/**
+ *
+ * @author Intissar BENAZIZ
+ */
+//Backing bean de la page customerList.xhtml
+@Named(value = "customerBean")
+@ViewScoped
+public class CustomerBean implements Serializable {
+
+    private List<Customer> customerList;
+
+    @Inject
+    private CustomerManager customerManager;
+
+    /**
+     * Creates a new instance of CustomerBean
+     */
+    public CustomerBean() {
+    }
+
+    //Retourne la liste des clients pour affichage dans une DataTable.
+    public List<Customer> getCustomers() {
+        if (customerList == null) {
+            customerList = customerManager.getAllCustomers();
+        }
+        return customerList;
+    }
+}
