60207b5e443765baa8366e8e209a03863a1cc291
Intissar BENAZIZ <Intissar BENAZIZ@DESKTOP-RRJUMKP>
2023-10-29 19:19:55 +0100

    Affichage des details d'un client

--- src/main/java/rbt/emsi/benaziz/tpc/tpcustomerbenaziz/service/CustomerManager.java
+++ src/main/java/rbt/emsi/benaziz/tpc/tpcustomerbenaziz/service/CustomerManager.java
@@ -39,4 +39,8 @@ public class CustomerManager {
     public void persist(Customer customer) {
         em.persist(customer);
     }
+
+    public Customer findById(int idCustomer) {
+        return em.find(Customer.class, idCustomer);
+    }
 }
